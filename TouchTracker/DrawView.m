d3f84dca661bb2d965651ee064a555cf93c88eb6
cocos543 <543314602@qq.com>
2015-12-02 18:04:58 +0800

    完成12章 多点触摸绘图

--- TouchTracker/DrawView.m
+++ TouchTracker/DrawView.m
@@ -14,6 +14,12 @@
 @property (nonatomic,strong) Line *currentLine;
 @property (nonatomic,strong) NSMutableArray *finishedLines;
 
+/*多点同时划线,要解决两个问题
+ *1 要能同时保存多个触摸事件都应的起点位置
+ *2 多个事件的终点和已经记录的起点要一一对应不能混淆
+ */
+@property (nonatomic,strong) NSMutableDictionary *linesProgress;
+
 @end
 
 @implementation DrawView
@@ -22,8 +28,10 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        self.linesProgress = [[NSMutableDictionary alloc] init];
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
+        self.multipleTouchEnabled = YES;
     }
     return self;
 }
@@ -44,32 +52,74 @@
     for (Line *l in self.finishedLines) {
         [self strokeLine:l];
     }
-    if (self.currentLine) {
+    if (self.linesProgress.count > 0) {
         [[UIColor redColor] set];
-        [self strokeLine:self.currentLine];
+        for (NSValue *key in self.linesProgress) {
+            [self strokeLine:self.linesProgress[key]];
+        }
     }
+//    if (self.currentLine) {
+//        [[UIColor redColor] set];
+//        [self strokeLine:self.currentLine];
+//    }
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    UITouch *t = [touches anyObject];
-    CGPoint location = [t locationInView:self];
-    self.currentLine = [[Line alloc] init];
-    self.currentLine.begin = location;
-    self.currentLine.end = location;
+    //向控制台打印触摸事件
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for (UITouch *t in touches) {
+        CGPoint location = [t locationInView:self];
+        Line *l = [[Line alloc] init];
+        l.begin = location;
+        l.end = location;
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        self.linesProgress[key] = l;
+    }
+//    UITouch *t = [touches anyObject];
+//    CGPoint location = [t locationInView:self];
+//    self.currentLine = [[Line alloc] init];
+//    self.currentLine.begin = location;
+//    self.currentLine.end = location;
     [self setNeedsDisplay];
 }
 
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    UITouch *t = [touches anyObject];
-    CGPoint location = [t locationInView:self];
-    self.currentLine.end = location;
+    //向控制台打印触摸事件
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for (UITouch *t in touches) {
+        CGPoint location = [t locationInView:self];
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        Line *l = self.linesProgress[key];
+        l.end = location;
+    }
+//    UITouch *t = [touches anyObject];
+//    CGPoint location = [t locationInView:self];
+//    self.currentLine.end = location;
     [self setNeedsDisplay];
 }
 
 -(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self.finishedLines addObject:self.currentLine];
-    self.currentLine = nil;
+    //向控制台打印触摸事件
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.finishedLines addObject:self.linesProgress[key]];
+        [self.linesProgress removeObjectForKey:key];
+    }
+//    [self.finishedLines addObject:self.currentLine];
+//    self.currentLine = nil;
     [self setNeedsDisplay];
 }
 
+//触摸突然被中断时.
+-(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //向控制台打印触摸事件
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.linesProgress removeObjectForKey:key];
+    }
+    
+}
 @end
+
